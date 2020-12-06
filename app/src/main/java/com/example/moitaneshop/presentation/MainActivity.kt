1a9c14c935f010c6068163303e3273ee5f19ee3e
Rogavactive <droga16@freeuni.edu.ge>
2020-12-06 08:24:04 +0400

    added adapter to recycler

--- app/src/main/java/com/example/moitaneshop/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moitaneshop/presentation/MainActivity.kt
@@ -2,18 +2,20 @@ package com.example.moitaneshop.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
-import com.example.moitaneshop.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moitaneshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val adapter: ShopItemRecyclerAdapter = ShopItemRecyclerAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.recyclerShopList.layoutManager = LinearLayoutManager(this)
+        binding.recyclerShopList.adapter = adapter
         initListeners()
         setContentView(binding.root)
     }
